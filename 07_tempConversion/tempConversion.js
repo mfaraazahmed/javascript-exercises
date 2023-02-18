d2bc0cea38b218f91e13f0105ff8934d365bf661
Mohammed Faraaz Ahmed <91116035+mfaraazahmed@users.noreply.github.com>
2023-02-18 18:02:24 +0530

    Completed assignment 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  if (typeof fahrenheit !== 'number') {
+    return 'ERROR';
+  }
+
+  return parseFloat(((fahrenheit - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  if (typeof celsius !== 'number') {
+    return 'ERROR';
+  }
+
+  return parseFloat((celsius * 9 / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line

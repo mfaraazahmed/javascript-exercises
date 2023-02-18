d832fa56089a7e73401158dff5e5b7fd44b15876
Mohammed Faraaz Ahmed <91116035+mfaraazahmed@users.noreply.github.com>
2023-02-18 16:12:44 +0530

    completed assignment 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(strn, num) {
+    if(num<0){
+        return 'ERROR'
+    }
+    let repeatedStr = ''
+    for(let i=0; i<num; i++){
+        repeatedStr += strn;
+    }
 
+    return repeatedStr;
 };
 
 // Do not edit below this line

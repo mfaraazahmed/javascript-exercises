bbcc880a27eea326eb50429fb9e048cb5f5469b2
Mohammed Faraaz Ahmed <91116035+mfaraazahmed@users.noreply.github.com>
2023-02-18 17:45:48 +0530

    completed assignment 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
 
+    if(numOne < 0 || numTwo < 0){
+        return 'ERROR';
+    }
+
+    if (numOne > numTwo) {
+    [numOne, numTwo] = [numTwo, numOne];
+    }
+
+    if (typeof numOne !== 'number' || typeof numTwo !== 'number') {
+        return 'ERROR';
+    }
+
+    for(let i=numOne; i<=numTwo; i++){
+        sum +=i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
